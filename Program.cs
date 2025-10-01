567248db16d219d1099cadfc5c8229129f87799b
Any-utka <annadotsen@mail.ru>
2025-10-01 21:23:30 +0300

    Initial commit with project files and .gitignore

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿Console.WriteLine("Hello, Jerry");
+
+
+void PrintABC() {
+    Console.WriteLine("A");
+    Console.WriteLine("B");
+    Console.WriteLine("C");
+}
+
+for (int i = 0; i<3; i++)
+{
+    PrintABC();
+    Thread.Sleep(500);
+}
+
+void C()
+{
+    Console.WriteLine("C");
+}
+
+void B()
+{
+    Console.WriteLine("B");
+}
+
+void A()
+{
+    Console.WriteLine("A start");
+    B();
+    C();
+    Console.WriteLine("A end");
+}
+
+A();
+A();
